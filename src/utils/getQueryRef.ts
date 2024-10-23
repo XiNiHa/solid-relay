ec2bce172e2af4287f3121590f4b905dbc78d53d
Iha Shin <me@xiniha.dev>
2024-10-23 13:12:35 +0900

    fix: use observeFragment also on queries

---
+++ src/utils/getQueryRef.ts
@@ -0,0 +1,11 @@
+import RelayRuntime, { type OperationDescriptor } from "relay-runtime";
+import type { KeyType } from "relay-runtime/lib/store/ResolverFragments";
+
+export const getQueryRef = (operation: OperationDescriptor) =>
+	({
+		[RelayRuntime.ID_KEY]: operation.fragment.dataID,
+		[RelayRuntime.FRAGMENTS_KEY]: {
+			[operation.fragment.node.name]: operation.request.variables,
+		},
+		[RelayRuntime.FRAGMENT_OWNER_KEY]: operation.request,
+	}) as unknown as KeyType;
