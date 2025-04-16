cc7a4873b0fad77c9fdaa33bfdad36854776f220
Iha Shin <me@xiniha.dev>
2025-04-16 20:24:08 +0900

    chore: use vite-plugin-cjs-interop in example

--- example/app.config.ts
+++ example/app.config.ts
@@ -1,18 +1,14 @@
 import { defineConfig } from "@solidjs/start/config";
 import solidDevtools from "solid-devtools/vite";
+import { cjsInterop } from "vite-plugin-cjs-interop";
 import relay from "vite-plugin-relay";
 
 export default defineConfig({
-	vite: {
-		plugins: [solidDevtools({ autoname: true }), relay],
-		optimizeDeps: {
-			include: ["relay-runtime"],
-		},
-		ssr: {
-			noExternal: [/^relay-runtime(?:\/|$)/, /^solid-js(?:\/|$)/],
-			optimizeDeps: {
-				include: ["relay-runtime", "relay-runtime/experimental"],
-			},
-		},
-	},
+	vite: () => ({
+		plugins: [
+			solidDevtools({ autoname: true }),
+			relay,
+			cjsInterop({ dependencies: ["relay-runtime"] }),
+		],
+	}),
 });
